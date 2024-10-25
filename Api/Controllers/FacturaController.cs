9db3c28926d29296cf4dfb028a1bd55b59ffb173
Brian morales <briandv2@hotmail.com>
2024-10-25 16:23:25 -0500

    subir api

---
+++ Api/Controllers/FacturaController.cs
@@ -0,0 +1,48 @@
+﻿using Core.Dtos;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FacturaController : ControllerBase
+    {
+        private readonly IFacturaService _facturaService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FacturaController"/> class.
+        /// </summary>
+        /// <param name="propertyService">The property service.</param>
+        public FacturaController(IFacturaService facturaService)
+        {
+            _facturaService = facturaService;
+        }
+
+        /// <summary>
+        /// Creates a new property.
+        /// </summary>
+        /// <param name="propertyDto">The property DTO.</param>
+        /// <returns>An <see cref="IActionResult"/> representing the result of the action.</returns>
+        [HttpPost]
+        public async Task<IActionResult> CrearFactura([FromBody] FacturaDTO facturaDTO)
+        {
+            if (facturaDTO == null)
+                return BadRequest("Factura es requerido.");
+
+            try
+            {
+                await _facturaService.CrearFactura(facturaDTO);
+                return Ok("factura creada correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+     
+    }
+}
